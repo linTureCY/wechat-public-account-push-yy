8f6f9f57b8530041456418bbc919e8a1fbeb9a94
linTureCY <103411615+linTureCY@users.noreply.github.com>
2022-10-24 16:25:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'okPjp535PoMS0HDqnOqtTMzttyV8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '11-01',
+      useTemplateId: '0MDK1hoCchsins4gOxT1iYML1wrP8QC6dED_zqEJGIE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-01',
       festivals: [
